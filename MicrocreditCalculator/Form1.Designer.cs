18e130442d9e6cc227d6bac8cdcd037bb56bb871
Антон Ахтямов <yznikfortyni@gmail.com>
2021-11-13 12:42:44 +0300

    .

--- MicrocreditCalculator/Form1.Designer.cs
+++ MicrocreditCalculator/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.Label Title;
             this.OpenFileButton = new System.Windows.Forms.Button();
             this.SaveReportButton = new System.Windows.Forms.Button();
             this.PutButton = new System.Windows.Forms.Button();
@@ -39,20 +38,10 @@
             this.Bet = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            Title = new System.Windows.Forms.Label();
+            this.Heading = new System.Windows.Forms.Label();
+            this.WriteLoanButton = new System.Windows.Forms.Button();
+            this.FinishTyping = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // Title
-            // 
-            Title.Dock = System.Windows.Forms.DockStyle.Top;
-            Title.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            Title.ForeColor = System.Drawing.Color.Green;
-            Title.Location = new System.Drawing.Point(0, 0);
-            Title.Name = "Title";
-            Title.Size = new System.Drawing.Size(802, 48);
-            Title.TabIndex = 3;
-            Title.Text = "Microcredit Calculator";
-            Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // OpenFileButton
             // 
@@ -72,6 +61,7 @@
             this.SaveReportButton.TabIndex = 1;
             this.SaveReportButton.Text = "Save Report";
             this.SaveReportButton.UseVisualStyleBackColor = true;
+            this.SaveReportButton.Click += new System.EventHandler(this.SaveReportButton_Click);
             // 
             // PutButton
             // 
@@ -141,6 +131,38 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // Heading
+            // 
+            this.Heading.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Heading.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Heading.ForeColor = System.Drawing.Color.Green;
+            this.Heading.Location = new System.Drawing.Point(0, 0);
+            this.Heading.Name = "Heading";
+            this.Heading.Size = new System.Drawing.Size(802, 30);
+            this.Heading.TabIndex = 11;
+            this.Heading.Text = "Microcredit Calculator";
+            this.Heading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // WriteLoanButton
+            // 
+            this.WriteLoanButton.Location = new System.Drawing.Point(353, 122);
+            this.WriteLoanButton.Name = "WriteLoanButton";
+            this.WriteLoanButton.Size = new System.Drawing.Size(75, 23);
+            this.WriteLoanButton.TabIndex = 12;
+            this.WriteLoanButton.Text = "WriteLoan";
+            this.WriteLoanButton.UseVisualStyleBackColor = true;
+            this.WriteLoanButton.Click += new System.EventHandler(this.WriteLoanButton_Click);
+            // 
+            // FinishTyping
+            // 
+            this.FinishTyping.Location = new System.Drawing.Point(346, 257);
+            this.FinishTyping.Name = "FinishTyping";
+            this.FinishTyping.Size = new System.Drawing.Size(88, 23);
+            this.FinishTyping.TabIndex = 13;
+            this.FinishTyping.Text = "Finish Typing";
+            this.FinishTyping.UseVisualStyleBackColor = true;
+            this.FinishTyping.Click += new System.EventHandler(this.FinishTyping_Click);
+            // 
             // Form1
             // 
             this.AcceptButton = this.PutButton;
@@ -148,6 +170,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(802, 291);
+            this.Controls.Add(this.FinishTyping);
+            this.Controls.Add(this.WriteLoanButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.Bet);
             this.Controls.Add(this.label3);
@@ -157,7 +181,7 @@
             this.Controls.Add(this.PutButton);
             this.Controls.Add(this.SaveReportButton);
             this.Controls.Add(this.OpenFileButton);
-            this.Controls.Add(Title);
+            this.Controls.Add(this.Heading);
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -179,6 +203,9 @@
         private System.Windows.Forms.TextBox Bet;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label Heading;
+        private System.Windows.Forms.Button WriteLoanButton;
+        private System.Windows.Forms.Button FinishTyping;
     }
 }
 
--- MicrocreditCalculator/Form1.cs
+++ MicrocreditCalculator/Form1.cs
@@ -21,8 +21,8 @@ namespace MicrocreditCalculator
         }
 
         public string input;
-        public string StartTime;
-        public string EndTime;
+        public string loanSumPer;
+        public string loanTermPer;
 
         private void OpenFileButton_Click(object sender, EventArgs e)
         {
@@ -44,13 +44,120 @@ namespace MicrocreditCalculator
 
         private void PutButton_Click(object sender, EventArgs e)
         {
-            try 
-            { 
-                int x = Int32.Parse(Bet.Text);
+            try
+            {
+                input += Int32.Parse(Bet.Text) + "\n";
+                Bet.Text = "";
+
+                Heading.Text = "Microcredit Calculator";
+                Heading.ForeColor = Color.Green;
+
             }
 
+            catch 
+            {
+                Heading.Text = "Invalid input format";
+                Heading.ForeColor = Color.Red;
+            }
+     
+        }
 
-            
+        private void WriteLoanButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double qwe = Double.Parse(LoanSum.Text);
+                int asd = Int32.Parse(LoanTerm.Text);
+
+                loanSumPer = LoanSum.Text;
+                loanTermPer = LoanTerm.Text;
+
+                input += loanSumPer + '\n';
+                input += loanTermPer + '\n';
+
+                Heading.Text = "Microcredit Calculator";
+                Heading.ForeColor = Color.Green;
+            }
+
+            catch
+            {
+                Heading.Text = "Invalid input format";
+                Heading.ForeColor = Color.Red;
+            }
+        }
+
+        private void FinishTyping_Click(object sender, EventArgs e)
+        {
+            input = input.TrimEnd('\n');
+            var rs = new StreamWriter(openFileDialog1.FileName, false);
+            rs.WriteLine(input);
+            rs.Close();
+        }
+
+        private void SaveReportButton_Click(object sender, EventArgs e)
+        {
+            var sr = new StreamReader(openFileDialog1.FileName);
+            int lenfile = sr.ReadToEnd().Split('\n').Length;
+            sr = new StreamReader(openFileDialog1.FileName);
+
+            double loanSumPer0 = 0;
+            int loanTermPer0 = 0;
+
+
+            ArrayList betArray = new ArrayList();
+
+            for (int i = 0; i < lenfile; i++)
+            {
+                string inputline = sr.ReadLine();
+                if (inputline != "\n" && inputline != "" && inputline != null)
+                {
+                    if (i == 0)
+                    {
+                        loanSumPer0 = Double.Parse(inputline);
+                    }
+
+                    else if (i == 1)
+                    {
+                        loanTermPer0 = Int32.Parse(inputline);
+                    }
+
+                    else
+                    {
+                        betArray.Add(Double.Parse(inputline));
+                    }
+                }
+            }
+
+            ArrayList Cumulative = new ArrayList();
+            ArrayList PayoutAmount = new ArrayList();
+
+            double CumulativePer = 0;
+            double PayoutAmountPer = 0;
+            PayoutAmountPer += loanSumPer0;
+
+            foreach (double i in betArray)
+            {
+                CumulativePer += (loanSumPer0 * i) / 100;
+                Cumulative.Add(CumulativePer);
+
+                PayoutAmountPer = loanSumPer0 + CumulativePer;
+                PayoutAmount.Add(PayoutAmountPer);
+
+            }
+
+            double TotalPayoutAmount = PayoutAmountPer;
+            double amountInterest = CumulativePer;
+            double EffectiveBid = amountInterest / loanSumPer0 / loanTermPer0;
+
+
+            string otchet = Convert.ToString(TotalPayoutAmount) + '\n' + Convert.ToString(amountInterest) + '\n' + Convert.ToString(EffectiveBid * 100) + '\n';
+
+            for (int i = 0; i < Cumulative.Count; i++)
+            {
+                otchet += Convert.ToString(Cumulative[i]) + "; " + Convert.ToString(PayoutAmount[i]) + '\n';
+            }
+
+            System.IO.File.WriteAllText("C:\\Users\\201904\\Documents\\Report.txt", otchet);
         }
     }
 }
